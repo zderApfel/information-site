56ac0b25f89be8da38eea163ec960086e42ebfa1
zderApfel <scoutt172@yahoo.com>
2020-12-05 06:26:06 -0500

    Opens index.html on page load

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-const http = require("http");
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
+fs.readFile("index.html", function(err, data){
+    if (err){
+        res.writeHead(400, {"Content-Type": "text/html"});
+        return res.end("404 Not Found");
+    }
+});
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
