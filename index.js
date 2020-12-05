e1f1aeaec7d7fec11361921510149782566f00ab
zderApfel <scoutt172@yahoo.com>
2020-12-05 06:31:16 -0500

    No it actually doesn't and I have no idea what I'm doing

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
-fs.readFile("index.html", function(err, data){
-    if (err){
-        res.writeHead(400, {"Content-Type": "text/html"});
-        return res.end("404 Not Found");
-    }
-});
-
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
